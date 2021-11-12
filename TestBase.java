234bcaf3c6fc37439265beed01a883d58ce9c1e3
senbinkop66 <2507019430@qq.com>
2021-11-12 19:35:05 +0800

    update

--- TestBase.java
+++ TestBase.java
@@ -1,7 +1,19 @@
+import java.util.*;
+import java.math.*;
 
 public class TestBase{
-	public static void main(String[] args) {
-		
+	public static void main(String[] args){
+		if (args.length==0 || args[0].equals("-h")) {
+			System.out.println("Hello,");
+		}else if(args[0].equals("-g")){
+			System.out.println("Goodbye,");
+		}
+		for (int i=1; i<args.length; i++) {
+			System.out.println(" "+args[i]);
+		}
+
+		//java TestBase -g senbin 36
+		//java TestBase -h senbin 36
 
 
 
