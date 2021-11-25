d7e5806c6c2ced3b1001b294a7cbc977cb1ef950
senbinkop66 <2507019430@qq.com>
2021-11-25 10:18:15 +0800

    update

--- GenericReflectionTest.java
+++ GenericReflectionTest.java
@@ -26,6 +26,7 @@ class Pair<T>{
 		second=newValue;
 	}
 }
+
 class ArrayAlg{
 	public static boolean hasNulls(Pair<?> p){
 		return p.getFirst()==null  || p.getSecond()==null;
@@ -61,10 +62,14 @@ public class GenericReflectionTest{
 		String name=m.getName();
 		System.out.print(Modifier.toString(m.getModifiers()));
 		System.out.print(" ");
-		printTypes(m.getTypeParameters(),false);
+		//getTypeParameters()  如果这个方法被声明为泛型方法， 则获得泛型类型变量，否则返回长度为 0 的数组。
+		printTypes(m.getTypeParameters(),"<",", ","> ",false);
+		//getGenericReturnType()  获得这个方法被声明的泛型返回类型。
+		printType(m.getGenericReturnType(),false);
 		System.out.print(" ");
 		System.out.print(name);
 		System.out.print("(");
+		//getGenericParameterTypes()  获得这个方法被声明的泛型参数类型。 如果这个方法没有参数， 返回长度为 0 的数组。
 		printTypes(m.getGenericParameterTypes(),"",", ","",false);
 		System.out.println(")");
 	}
@@ -84,46 +89,63 @@ public class GenericReflectionTest{
 	public static void printType(Type type,boolean isDefinition){
 		if (type instanceof Class) {
 			Class<?> t=(Class<?>) type;
+			//getName()  获得类型变量的名字。
 			System.out.print(t.getName());
 		}else if (type instanceof TypeVariable) {
 			TypeVariable<?> t=(TypeVariable<?>) type;
 			System.out.print(t.getName());;
 			if (isDefinition) {
+				//getBounds()  获得类型变量的子类限定，否则， 如果该变量无限定， 则返回长度为 0 的数组。
 				printTypes(t.getBounds()," extends ","& ","",false);
-			}else if (type instanceof WildcardType) {
-				WildcardType t=(WildcardType) type;
-				System.out.print("?");
-				printTypes(t.getUpperBound()," extends ","& ",""<false);
-				printTypes(t.getLowerBound()," super ","& ","",false);
-			}else if (type instanceof ParameterizedType){
-				ParameterizedType t=(ParameterizedType) type;
-				Type owner=t.getOwnerType();
-				if (owner!=null) {
-					printType(owner,false);
-					System.out.print(".");
-				}
-				printType(t.getRawType(),false);
-				printTypes(t.getActualTypeArguments(),"<",", ",">",false);
-			}else if (type instanceof GenericArrayType){
-				GenericArrayType t=(GenericArrayType) type;
-				System.out.print("");
-				printType(t.getGenericComponentType(),isDefinition);
-				System.out.print("[]");
 			}
+		}else if (type instanceof WildcardType) {
+			WildcardType t=(WildcardType) type;
+			System.out.print("?");
+			//getUpperBounds()  获得这个类型变量的子类 ( extends) 限定，否则， 如果没有子类限定，则返回长度为0 的数组。
+			printTypes(t.getUpperBounds()," extends ","& ","",false);
+			//getLowerBounds()  获得这个类型变量的超类（super) 限定，否则， 如果没有超类限定， 则返回长度为 0的数组。
+			printTypes(t.getLowerBounds()," super ","& ","",false);
+		}else if (type instanceof ParameterizedType){
+			ParameterizedType t=(ParameterizedType) type;
+			//getOwnerType()  如果是内部类型， 则返回其外部类型， 如果是一个顶级类型， 则返回 null。
+			Type owner=t.getOwnerType();
+			if (owner!=null) {
+				printType(owner,false);
+				System.out.print(".");
+			}
+			//getRawType()  获得这个参数化类型的原始类型。
+			printType(t.getRawType(),false);
+			//getActualTypeArguments()  获得这个参数化类型声明时所使用的类型参数。
+			printTypes(t.getActualTypeArguments(),"<",", ",">",false);
+		}else if (type instanceof GenericArrayType){
+			GenericArrayType t=(GenericArrayType) type;
+			System.out.print("");
+			//getGenericComponentType()  获得声明该数组类型的泛型组件类型。
+			printType(t.getGenericComponentType(),isDefinition);
+			System.out.print("[]");
 		}
 	}
 
 	public static void main(String[] args) {
-		String name="Pair";  //类名
+		//类名
+		String name="Pair";
 		try{
 			Class<?> cl=Class.forName(name);
 			printClass(cl);
 			for (Method m:cl.getDeclaredMethods()){
 				printMethod(m);
 			}
-		}catch (ClassNotFoundException e){
+		}catch(ClassNotFoundException e){
 			e.printStackTrace();
 		}
-
 	}
 }
+/*
+Compiling GenericReflectionTest.java......
+------Output------
+class Pair<T> extendsjava.lang.Object
+public T getFirst()
+public T getSecond()
+public void setSecond(T)
+public void setFirst(T)
+*/
