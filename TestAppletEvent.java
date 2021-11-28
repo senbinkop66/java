a71e228b7ca9b04a3f7df176c95517cac1998256
senbinkop66 <2507019430@qq.com>
2021-11-28 21:32:46 +0800

    update

--- TestAppletEvent.java
+++ TestAppletEvent.java
@@ -81,12 +81,56 @@ class PlafFrame extends JFrame{
 	}
 }
 
+class ActionFrame extends JFrame{
+	private JPanel buttonPanel;
+	private static final int DEFAULT_WIDTH=600;
+	private static final int DEFAULT_HEIGHT=400;
+
+	public ActionFrame(){
+		setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+		buttonPanel=new JPanel();
+		
+		Action yellowAction=new ColorAction("Yellow",new ImageIcon("./image/yellow_icon.gif"),Color.YELLOW);
+		Action blueAction=new ColorAction("Blue",new ImageIcon("./image/blue_icon.gif"),Color.BLUE);
+		Action redAction=new ColorAction("Red",new ImageIcon("./image/red_icon.gif"),Color.RED);
+
+		buttonPanel.add(new JButton(yellowAction));
+		buttonPanel.add(new JButton(blueAction));
+		buttonPanel.add(new JButton(redAction));
+
+		add(buttonPanel);
+		//获得将按键映射到动作键的输入映射。
+		InputMap imap=buttonPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+		//将名 / 值对放置在动作对象内。
+		imap.put(KeyStroke.getKeyStroke("ctrl Y"),"panel.yellow"); //根据一个便于人们阅读的说明创建一个按键
+		imap.put(KeyStroke.getKeyStroke("ctrl B"),"panel.blue");
+		imap.put(KeyStroke.getKeyStroke("ctrl R"),"panel.red");
+
+		ActionMap amap=buttonPanel.getActionMap();  //返回关联动作映射键（可以是任意的对象）和动作对象的映射。
+		amap.put("panel.yellow",yellowAction);
+		amap.put("panel.blue",blueAction);
+		amap.put("panel.red",redAction);
+	}
+	public class ColorAction extends AbstractAction{
+		public ColorAction(String name,Icon icon,Color c){
+			putValue(Action.NAME,name);
+			putValue(Action.SMALL_ICON,icon);
+			putValue(Action.SHORT_DESCRIPTION,"Set panel color to "+name.toLowerCase());
+			putValue("color",c);
+		}
+		public void actionPerformed(ActionEvent event){
+			//返回被存储的名 / 值对的值。
+			Color c=(Color) getValue("color");
+			buttonPanel.setBackground(c);
+		}
+	}
+}
 public class TestAppletEvent{
 	public static void main(String[] args) {
 		EventQueue.invokeLater(()->{
-			JFrame frame=new PlafFrame();
+			JFrame frame=new ActionFrame();
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setTitle("改变观感");  //设置框架标题
+			frame.setTitle("动作事件");  //设置框架标题
 			frame.setLocation(50,50);
 			frame.setVisible(true);  //使其可见性
 		});
